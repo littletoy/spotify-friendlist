f614b17ce42ff5c47e8fb31cab93314b312fb7e9
Kangseon Cho <kangseoncho@Kangseons-MacBook-Air.local>
2017-03-31 18:51:36 -0700

    pushing up the test array

---
+++ arrayForTest.js
@@ -0,0 +1,122 @@
+[
+{
+"artist": "Luther Vandross",
+"songName": "If I Didn't Know Better",
+"duration": 247533,
+"uri": "spotify:track:6GBxfhz2eKwlF1OaYGy7Cx"
+},
+{
+"artist": "Marketa Irglova and Glen Hansard",
+"songName": "If You Want Me",
+"duration": 228786,
+"uri": "spotify:track:5cLCeJ0RHfLrP1pcRWoiFz"
+},
+{
+"artist": "Marketa Irglova and Glen Hansard",
+"songName": "If You Want Me",
+"duration": 228786,
+"uri": "spotify:track:5cLCeJ0RHfLrP1pcRWoiFz"
+},
+{
+"artist": "Marketa Irglova and Glen Hansard",
+"songName": "If You Want Me",
+"duration": 228786,
+"uri": "spotify:track:5cLCeJ0RHfLrP1pcRWoiFz"
+},
+{
+"artist": "Marketa Irglova and Glen Hansard",
+"songName": "If You Want Me",
+"duration": 228786,
+"uri": "spotify:track:5cLCeJ0RHfLrP1pcRWoiFz"
+},
+{
+"artist": "Marketa Irglova and Glen Hansard",
+"songName": "If You Want Me",
+"duration": 228786,
+"uri": "spotify:track:5cLCeJ0RHfLrP1pcRWoiFz"
+},
+{
+"artist": "Marketa Irglova and Glen Hansard",
+"songName": "If You Want Me",
+"duration": 228786,
+"uri": "spotify:track:5cLCeJ0RHfLrP1pcRWoiFz"
+},
+{
+"artist": "Marketa Irglova and Glen Hansard",
+"songName": "If You Want Me",
+"duration": 228786,
+"uri": "spotify:track:5cLCeJ0RHfLrP1pcRWoiFz"
+},
+{
+"artist": "Marketa Irglova and Glen Hansard",
+"songName": "If You Want Me",
+"duration": 228786,
+"uri": "spotify:track:5cLCeJ0RHfLrP1pcRWoiFz"
+},
+{
+"artist": "Marketa Irglova and Glen Hansard",
+"songName": "If You Want Me",
+"duration": 228786,
+"uri": "spotify:track:5cLCeJ0RHfLrP1pcRWoiFz"
+},
+{
+"artist": "Marketa Irglova and Glen Hansard",
+"songName": "If You Want Me",
+"duration": 228786,
+"uri": "spotify:track:5cLCeJ0RHfLrP1pcRWoiFz"
+},
+{
+"artist": "Marketa Irglova and Glen Hansard",
+"songName": "If You Want Me",
+"duration": 228786,
+"uri": "spotify:track:5cLCeJ0RHfLrP1pcRWoiFz"
+},
+{
+"artist": "Marketa Irglova and Glen Hansard",
+"songName": "If You Want Me",
+"duration": 228786,
+"uri": "spotify:track:5cLCeJ0RHfLrP1pcRWoiFz"
+},
+{
+"artist": "Marketa Irglova and Glen Hansard",
+"songName": "If You Want Me",
+"duration": 228786,
+"uri": "spotify:track:5cLCeJ0RHfLrP1pcRWoiFz"
+},
+{
+"artist": "Marketa Irglova and Glen Hansard",
+"songName": "If You Want Me",
+"duration": 228786,
+"uri": "spotify:track:5cLCeJ0RHfLrP1pcRWoiFz"
+},
+{
+"artist": "Marketa Irglova and Glen Hansard",
+"songName": "If You Want Me",
+"duration": 228786,
+"uri": "spotify:track:5cLCeJ0RHfLrP1pcRWoiFz"
+},
+{
+"artist": "Marketa Irglova and Glen Hansard",
+"songName": "If You Want Me",
+"duration": 228786,
+"uri": "spotify:track:5cLCeJ0RHfLrP1pcRWoiFz"
+},
+{
+"artist": "Marketa Irglova and Glen Hansard",
+"songName": "If You Want Me",
+"duration": 228786,
+"uri": "spotify:track:5cLCeJ0RHfLrP1pcRWoiFz"
+},
+{
+"artist": "Marketa Irglova and Glen Hansard",
+"songName": "If You Want Me",
+"duration": 228786,
+"uri": "spotify:track:5cLCeJ0RHfLrP1pcRWoiFz"
+},
+{
+"artist": "Adam Levine",
+"songName": "Lost Stars",
+"duration": 267933,
+"uri": "spotify:track:7rdGrVIoqwPWOULauvglio"
+}
+]
